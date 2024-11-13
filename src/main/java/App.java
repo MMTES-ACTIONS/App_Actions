1f2bb5a71aa66f5515ede1795540214bf755d1f9
fernandorufinobarcelos <129413508+fernandorufinobarcelos@users.noreply.github.com>
2024-11-13 19:45:57 -0300

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,1 +1,11 @@
-
+public class CalculadoraApp {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        System.out.println("Soma: " + calculadora.somar(10, 5));
+        System.out.println("Subtração: " + calculadora.subtrair(10, 5));
+        System.out.println("Multiplicação: " + calculadora.multiplicar(10, 5));
+        System.out.println("Divisão: " + calculadora.dividir(10, 5));
+        System.out.println("Divisão: " + calculadora.dividir(5, 5));
+        
+    }
+}
