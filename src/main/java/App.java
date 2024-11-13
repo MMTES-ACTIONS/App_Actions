a96a5cc0b0890080b24ed90d038a471f0f0cee2e
Rafaela Moreira <45953979+rafapcmor@users.noreply.github.com>
2024-11-13 20:19:18 -0300

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,7 +5,7 @@ public class App {
         System.out.println("Subtração: " + calculadora.subtrair(10, 5));
         System.out.println("Multiplicação: " + calculadora.multiplicar(10, 5));
         System.out.println("Divisão: " + calculadora.dividir(10, 5));
-        System.out.println("Divisão: " + calculadora.dividir(5, 5));
+        System.out.println("Divisão: " + calculadora.dividir(5, 5))
         
     }
 }
